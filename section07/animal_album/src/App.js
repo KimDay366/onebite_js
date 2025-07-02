a0f4e0dcfec015827609d9b05f6bb1e563671ad5
KimDay366 <kimjs910522@gmail.com>
2025-07-02 17:48:01 +0900

    Create App.js

---
+++ section07/animal_album/src/App.js
@@ -0,0 +1,8 @@
+import TabBar from "./components/tabBar";
+import Content from "./components/content";
+
+export default function App() {
+  const tabBar = new TabBar();
+  const content = new Content();
+  console.log(123);
+}
