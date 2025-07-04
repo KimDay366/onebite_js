a2b34cf9f330c47b734bf51b4f3d2ed6d0b71d12
KimDay366 <kimjs910522@gmail.com>
2025-07-04 15:16:35 +0900

    Create index.js

---
+++ section08/animal_album/src/index.js
@@ -0,0 +1,9 @@
+import App from "./App.js";
+
+const $app = document.getElementById("app");
+
+new App($app);
+
+// SPA = Single Page Application
+// 단일 페이지에서 사용자의 요구에 따라 움직이는 웹사이트
+// 페이지가 로딩되는 시간이 짧아 사용자의 경험이 좋아짐
