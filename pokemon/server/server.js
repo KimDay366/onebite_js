eeb14ce1fd312ee13b216b7810955e470371c2a8
KimDay366 <kimjs910522@gmail.com>
2025-07-06 15:26:54 +0900

    Create server.js

---
+++ pokemon/server/server.js
@@ -0,0 +1,15 @@
+// server setting
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 1001;
+
+app.use(express.static(path.join(__dirname, "..")));
+app.get("*/splat", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("OK!!!!");
+});
