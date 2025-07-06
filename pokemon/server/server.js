a70cfa492c86e7b3b3e9c084b93178ff0e1a9fd0
KimDay366 <kimjs910522@gmail.com>
2025-07-06 18:27:01 +0900

    Update server.js

--- pokemon/server/server.js
+++ pokemon/server/server.js
@@ -6,7 +6,8 @@ const app = express();
 const PORT = 1001;
 
 app.use(express.static(path.join(__dirname, "..")));
-app.get("*/splat", (req, res) => {
+
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
