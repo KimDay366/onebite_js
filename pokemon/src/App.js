1fc01605f96d80851afa4e75318f24d8991f67be
KimDay366 <kimjs910522@gmail.com>
2025-07-06 18:27:03 +0900

    Update App.js

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,12 +1,33 @@
-import { request } from "./components/api.js";
+// components
+import pokemonList from "./components/pokemonList.js";
+
+// modules
+import { pokemonApi } from "./modules/api.js";
 
 // 현재 상태 + 상태 변화, 각 컴포넌트 로드, 초기화(inti) 작업
-export default function App() {
+export default function App($app) {
+  const getSearchWorld = () => {
+    if (window.location.search.includes("search=")) {
+      return window.location.search.split("search=")[1];
+    }
+    return "";
+  };
+
   // 현재 상태 로드
-  this.state = {};
+  this.state = {
+    type: "",
+    pokemonList: [],
+    searchWord: getSearchWorld(),
+    currentPage: window.location.pathname,
+  };
 
   // 컴포넌트 로드 샘플
-  //   const poke = new Poke();
+  const pokemonList = new PokemonList({
+    type,
+    searchWord,
+    handleItemClick: () => {},
+    handleTypeClick: () => {},
+  });
 
   // 상태 변화 로드
   this.setState = (newState) => {
@@ -15,6 +36,10 @@ export default function App() {
 
   // 초기화 작업
   const init = () => {};
+
+  this.setState = (newState) => {
+    this.state = newState;
+  };
 
   init();
 }
