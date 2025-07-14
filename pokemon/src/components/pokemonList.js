4e51278a963f25151b26bc3128446480945e7f54
KimDay366 <kimjs910522@gmail.com>
2025-07-14 21:15:21 +0900

    Update pokemonList.js

--- pokemon/src/components/pokemonList.js
+++ pokemon/src/components/pokemonList.js
@@ -1,8 +1,8 @@
 import { setPokemonType } from "../modules/typeTag.js";
 
 export default function PokemonList({
-  type,
-  searchWord,
+  $app,
+  initialState,
   handleItemClick,
   handleTypeClick,
 }) {
@@ -15,25 +15,26 @@ export default function PokemonList({
   this.handleTypeClick = handleTypeClick;
 
   this.template = () => {
-    // html 코드는 아래와 같이 제공드립니다.
-    // 필요한 코드를 추가적으로 작성해 웹 사이트를 완성하세요.
+    //   // html 코드는 아래와 같이 제공드립니다.
+    //   // 필요한 코드를 추가적으로 작성해 웹 사이트를 완성하세요.
 
     let temp = `<div class="pokemon-wrapper">`;
 
     if (this.state) {
-      this.state.pokemons.forEach((ele) => {
+      this.state.forEach((ele) => {
         temp += `<div class="img-wrapper" id="img${ele.id}">
-                    <img src="${ele.img}" alt="${ele.name}"></img>
-                </div>
-                <div class="pokemon-info">
-                    <div class="index">No.${ele.id}</div>
-                    <div class="name">${ele.name}</div>
-                    <div class="type">${ele.type}</div> 
-                </div>`;
+                      <img src="${ele.img}" alt="${ele.name}"></img>
+                  </div>
+                  <div class="pokemon-info">
+                      <div class="index">No.${ele.id}</div>
+                      <div class="name">${ele.name}</div>
+                      <div class="type">${setPokemonType(ele.type)}</div>
+                  </div>`;
       });
+      temp += `</div>`;
     }
 
-    return (temp += `</div>`);
+    return temp;
   };
 
   this.render = () => {
