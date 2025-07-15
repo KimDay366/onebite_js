2f4d62b8c770e5be3949c2b245fa79c17c472520
kimDay <kimjs910522@gmail.com>
2025-07-15 21:55:31 +0900

    Update App.js

--- section09/Trip-Wiki/src/App.js
+++ section09/Trip-Wiki/src/App.js
@@ -6,15 +6,48 @@ import { request } from "./components/api.js";
 
 // 현재 상태 표기 + 변경값 확인, 각각의 컴포넌트 불러오기, 초기값(init) 셋팅 진행
 export default function App($app) {
+
+  // 웹페이지 새로고침 할 경우, 설정했던 sort & search가 url엔 남아 있지만 리스트는 바뀜. 
+  // 따라서 아래의 두가지 함수를 셋팅 한 뒤, 현재 상태의 값을 업데이트 해 주어야 함
+
+  const getSortBy = () =>{
+
+    if(window.location.search){
+      const sortText = window.location.search.split('sort=')[1].split('&')[0];
+      console.log(`getSortBy : ${sortText}`);
+      return sortText;
+    };
+
+    return 'total';
+  };
+
+  const getSearchWord = () => {
+
+    if(window.location.search && window.location.search.includes('search=')){
+      return window.location.search.split('search=')[1];
+    };
+
+    return '';
+  };
+
+
   // 현재 상태 표기
   this.state = {
     // 페이지 로드 조건의 우선 순위는 아래 적은 내용과 상관 없음
+    
     // 페이지 로드 조건1) 페이지 로드 시 어떤 인덱스 데이터부터 가져올지 결정
     startIdx: 0,
+    
     // 페이지 로드 조건2) '정렬'의 기본값 셋팅
-    sortBy: "",
+    // sortBy: "",
+    // 페이지 로드 조건2 추가 ) 새로고침 시 '정렬'의 설정값 제거 후 'total'로 셋팅
+    sortBy : getSortBy(),
+    
     // 페이지 로드 조건3) 검색한 값으로 필터링
-    searchWord: "",
+    // searchWord: "",
+    // 페이지 로드 조건3 추가 ) 새로고침 시 '검색값' 제거 
+    searchWord: getSearchWord(),
+    
     // 페이지 로드 조건4) 지역 선택에 대해 필터링
     region: "",
 
@@ -30,6 +63,8 @@ export default function App($app) {
       searchWord: this.state.searchWord,
     },
     handleSortChange: async (sortBy) => {
+
+      console.log(sortBy);
       // 정렬 기준에 따라 변경
       const pageUrl = `/${this.state.region}?sort=${sortBy}`; // URL 변겅
       history.pushState(
@@ -88,7 +123,10 @@ export default function App($app) {
     initialState: this.state.cities,
     handleLoadMore: async () => {
       // [더보기] 버튼을 눌렀을 때 기존은 유지하며, 추가로 cities를 40개씩 불러옴
+
       const newStartIndex = this.state.startIDX + 40;
+      // 가지고 올 시티의 숫자는 api.js에서 'start=index'구문을 통해 결정됨 = url로 결정
+
       const newCities = await request(
         newStartIndex,
         this.state.sortBy,
@@ -127,6 +165,8 @@ export default function App($app) {
       this.state.searchWord,
       this.state.region
     ); // 최초의 cities는 API 의 반환된 URL을 기반으로 데이터를 가지고 옴
+
+    console.log(`init : ${this.state.sortBy}`);
 
     this.setState({
       ...this.state, // 기존에 있던 다른 값은 유지
