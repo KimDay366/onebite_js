c3deb1bb38487434291eed23b92ec5df23a29e07
kimDay <kimjs910522@gmail.com>
2025-07-15 09:39:24 +0900

    Update pokemonList.js

--- pokemon/src/components/pokemonList.js
+++ pokemon/src/components/pokemonList.js
@@ -18,20 +18,21 @@ export default function PokemonList({
     //   // html 코드는 아래와 같이 제공드립니다.
     //   // 필요한 코드를 추가적으로 작성해 웹 사이트를 완성하세요.
 
-    let temp = `<div class="pokemon-wrapper">`;
+    let temp = "";
 
     if (this.state) {
       this.state.forEach((ele) => {
-        temp += `<div class="img-wrapper" id="img${ele.id}">
+        temp += `<div class="pokemon-wrapper">
+                  <div class="img-wrapper" id="img${ele.id}">
                       <img src="${ele.img}" alt="${ele.name}"></img>
                   </div>
                   <div class="pokemon-info">
                       <div class="index">No.${ele.id}</div>
                       <div class="name">${ele.name}</div>
                       <div class="type">${setPokemonType(ele.type)}</div>
-                  </div>`;
+                  </div>
+                </div>`;
       });
-      temp += `</div>`;
     }
 
     return temp;
