5500a1c9b046181a26e04230e0c4cc82ab595207
kimDay <kimjs910522@gmail.com>
2025-07-16 19:52:00 +0900

    Update api.js

--- section09/Trip-Wiki/src/components/api.js
+++ section09/Trip-Wiki/src/components/api.js
@@ -8,7 +8,8 @@ export const request = async (startIdx, region, sortBy, searchWord) => {
     console.log(`API url : ${url}`);
 
     
-    console.log(`API : ${sortBy}`);
+    console.log(`API sort : ${sortBy}`);
+     console.log(`API search : ${searchWord}`);
 
     // 지역을 선택 한 경우, 지역과 함께 처음 출력할 인덱스 번호를 전달
     if (region && region !== "All") {
@@ -20,8 +21,6 @@ export const request = async (startIdx, region, sortBy, searchWord) => {
     // 정렬과 지역의 경우 값이 있을 때만 URL에 붙임
     if (sortBy) {
       url += `&sort=${sortBy}`;
-
-    console.log(`API : ${sortBy}`)
     }
     if (searchWord) {
       url += `&search=${searchWord}`;
